6a500870e62e3ce2d6e26ab720774deca010019c
Annette Iglesias <iglesiasannette@gmail.com>
2019-10-18 15:03:35 -0500

    Page Counter Exercise Bonus Complete

--- src/main/java/PageViewCounterServlet.java
+++ src/main/java/PageViewCounterServlet.java
@@ -5,17 +5,28 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/pageviewcounter")
+@WebServlet("/count")
 public class PageViewCounterServlet extends HttpServlet {
 
     static int counter = 0;
     @Override
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String output = String.format("This is response %d%n", counter++);
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println(output);
+        String reset = request.getParameter("reset");
+        if ("reset".equalsIgnoreCase(reset)) {
+            counter = 0;
+            String output = String.format("Counter has been reset to %d%n", counter);
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println(output);
+            counter++;
+        } else {
+            String output = String.format("This is response %d%n", counter++);
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println(output);
+        }
+
     }
 
 }
