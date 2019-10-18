a951a7fd073053e3e87b6152b20b041d29fc0c1c
Annette Iglesias <iglesiasannette@gmail.com>
2019-10-18 12:12:22 -0500

    Servlets Bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,12 +7,25 @@ import java.io.PrintWriter;
 
 @WebServlet("/hello-world")
 public class HelloWorldServlet extends HttpServlet {
+//
+//        @Override
+//        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//            response.setContentType("text/html");
+//            PrintWriter out = response.getWriter();
+//            out.println("<h1>Hello, World!</h1>");
+//        }
 
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+//    Bonus
+//
+//    Make the page say "Hello, <name>!" if name is passed as part of the query string
+//            (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            String name = request.getParameter("name");
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
-            out.println("<h1>Hello, World!</h1>");
-        }
-
+            out.println("<h1>Hello, World!</h1>" +
+                "<p> Here is a " + name + "</p>");
+    }
 }
